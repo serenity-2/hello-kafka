9e2afff519b0fbd5188d01995dd6e8dc266c5c0e
qianchangqing.ext <756974950@qq.com>
2021-02-19 17:42:42 +0800

    update files

--- src/main/java/com/jzjr/kafka/controller/ConsumerClient.java
+++ src/main/java/com/jzjr/kafka/controller/ConsumerClient.java
@@ -8,7 +8,7 @@ import org.springframework.stereotype.Component;
 public class ConsumerClient {
  @KafkaListener(topics="secret")
     public void listen(String msg){
-     System.out.println("receive message :");
+     System.out.println("receive message is:");
      System.out.println(msg);
  }
 
--- src/main/java/com/jzjr/kafka/controller/ProducerController.java
+++ src/main/java/com/jzjr/kafka/controller/ProducerController.java
@@ -1,9 +1,6 @@
 package com.jzjr.kafka.controller;
 
-import org.apache.kafka.common.protocol.types.Field;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.kafka.core.KafkaTemplate;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
